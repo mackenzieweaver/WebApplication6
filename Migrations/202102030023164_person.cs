6b00da8e45cd072c1347829b652e8e233833be64
Mack <macknz7@gmail.com>
2021-02-02 22:03:00 -0800

    Initial commit

---
+++ Migrations/202102030023164_person.cs
@@ -0,0 +1,27 @@
+namespace WebApplication6.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class person : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.People",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.People");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# WebApplication6
+ ASP.NET with .NET Framework Individual user accounts SQL Db access
